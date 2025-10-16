59fa88179484da89ac281a588ac7adb205ec101d
Nidhi Chandrakar <nidhichandrakar@hotmail.com>
2025-10-16 16:06:13 +0530

    Third Commit

--- text.js
+++ text.js
@@ -3,3 +3,5 @@ console.log("Hello World!");
 console.log("Goodbye");
 console.log("Hello, Good Morning!");
 console.log("How are you?");
+console.log("Give me some code");
+console.log("I am fine");
